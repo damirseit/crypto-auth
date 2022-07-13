7c2a6141f623f2c445913b11febd7058d54a30eb
Damir Seit <damir.seit.00@gmail.com>
2022-07-13 11:22:57 +0600

    Initial commit

---
+++ crypto-auth/src/main/java/com/example/cryptoauth/CryptoAuthApplication.java
@@ -0,0 +1,13 @@
+package com.example.cryptoauth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CryptoAuthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CryptoAuthApplication.class, args);
+	}
+
+}
